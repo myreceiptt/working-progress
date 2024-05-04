04fce6ee344f6e395a0356fce3349d93876d6ce5
myreceiptt <nota@straight-line.org>
2024-05-05 06:58:00 +0700

    <br />

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          It's my working progress, it's not my work in progress. If my work on{" "}
+          It's my working progress, it's not my work in progress.<br />If my work on{" "}
           <Link
             target="_blank"
             href="https://iqraa.straight-line.org/the-kings"
