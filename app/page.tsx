e1c175a834c811b8ec09092315243e07a8cc5fe5
myreceiptt <nota@straight-line.org>
2024-05-05 07:35:48 +0700

    image

--- app/page.tsx
+++ app/page.tsx
@@ -29,8 +29,13 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
+        quantity={474}
       />
+      
+      <Image
+        url="https://prompt.straight-line.org/prof-nota-v.1.20.jpg"
+      />
+
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Prof. NOTA's WP
       </h1>
