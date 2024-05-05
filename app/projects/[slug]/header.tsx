c3d6b39313efbd185a608fb08edfbc0ce767e056
myreceiptt <nota@straight-line.org>
2024-05-05 21:30:59 +0700

    thumbs

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useRef, useState } from "react";
 
 type Props = {
 	project: {
-		thumb?: string;
 		url?: string;
 		title: string;
 		description: string;
@@ -18,13 +17,6 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 	const ref = useRef<HTMLElement>(null);
 	const [isIntersecting, setIntersecting] = useState(true);
 
-	const images: { alt: string; src: string }[] = [];
-	if (project.thumb) {
-		images.push({
-			alt: project.title,
-			src: `https://prompt.straight-line.org/${project.thumb}`,
-		});
-	}
 	const links: { label: string; href: string }[] = [];
 	if (project.repository) {
 		links.push({
@@ -113,16 +105,6 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 						<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
 							{project.title}
 						</h1>
-						
-						{images.map((image) => (
-								<Image
-									src={image.src}
-									// width={474}
-									// height={474}
-									alt={image.alt}
-							  	/>
-						))}
-
 						<p className="mt-6 text-lg leading-8 text-zinc-300">
 							{project.description}
 						</p>
