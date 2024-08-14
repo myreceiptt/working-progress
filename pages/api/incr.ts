62c0a07d8924c56fc176b904fbae2bb894c068d9
myreceiptt <nota@straight-line.org>
2024-08-14 20:47:12 +0700

    0002

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -42,6 +42,7 @@ export default async function incr(req: NextRequest): Promise<NextResponse> {
       new NextResponse(null, { status: 202 });
     }
   }
-  await redis.incr(["pageviews", "projects", slug].join(":"));
+  await redis.incr(["pageviews", "progresses", slug].join(":"));
+  await redis.incr(["pageviews", "stories", slug].join(":"));
   return new NextResponse(null, { status: 202 });
 }
