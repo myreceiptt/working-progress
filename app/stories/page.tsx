a28e89e5e7f0a87bbf492a02e491a9082c8cb40e
myreceiptt <nota@straight-line.org>
2024-08-24 02:30:28 +0700

    pb

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -42,13 +42,13 @@ export default async function StoriesPage() {
   return (
     <div className="relative pb-16">
       <Navigation />
-      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+      <div className="px-6 pt-20 pb-10 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 md:pb-12 lg:pt-32 lg:pb-16">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
             0101 Stories
           </h2>
           <p className="mt-4 text-zinc-400">
-            Some 0101 initial stories in{" "}
+            Some 0101 stories in{" "}
             <Link
               target="_blank"
               href="#"
