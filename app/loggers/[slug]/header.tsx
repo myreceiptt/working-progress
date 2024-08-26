b3c93a98c8cb44f13c525688601e5f8da0b7a8fa
myreceiptt <nota@straight-line.org>
2024-08-26 22:25:02 +0700

    Loggers

--- app/loggers/[slug]/header.tsx
+++ app/loggers/[slug]/header.tsx
@@ -7,7 +7,6 @@ type Props = {
   logger: {
     title: string;
     description: string;
-    date: string;
   };
 
   views: number;
@@ -103,14 +102,6 @@ export const Header: React.FC<Props> = ({ logger, views }) => {
             <p className="mt-6 text-lg leading-8 text-zinc-300">
               {logger.description}
             </p>
-          </div>
-
-          <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-            <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-              <span aria-hidden="true">&rarr;</span>
-              {logger.date}
-              <span aria-hidden="true">&larr;</span>
-            </div>
           </div>
         </div>
       </div>
