8e81f85fc1fbe63870293bdb8afd247a660c6f59
myreceiptt <nota@straight-line.org>
2024-09-12 15:07:36 +0700

    log of 20220101

--- app/loggers/page.tsx
+++ app/loggers/page.tsx
@@ -24,13 +24,19 @@ export default async function LoggersPage() {
   const featured = allLoggers.find((logger) => logger.slug === "19920229")!;
   const top2 = allLoggers.find((logger) => logger.slug === "20160229")!;
   const top3 = allLoggers.find((logger) => logger.slug === "20200229")!;
+  const running = allLoggers.find((logger) => logger.slug === "running")!;
+  const bottom3 = allLoggers.find((logger) => logger.slug === "20440229")!;
+  const bottom2 = allLoggers.find((logger) => logger.slug === "21480229")!;
   const sorted = allLoggers
     .filter((p) => p.published)
     .filter(
       (logger) =>
         logger.slug !== featured.slug &&
         logger.slug !== top2.slug &&
-        logger.slug !== top3.slug
+        logger.slug !== top3.slug &&
+        logger.slug !== running.slug &&
+        logger.slug !== bottom3.slug &&
+        logger.slug !== bottom2.slug
     )
     .sort(
       (a, b) =>
@@ -98,7 +104,6 @@ export default async function LoggersPage() {
               </article>
             </Link>
           </Card>
-
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
             {[top2, top3].map((logger) => (
               <Card key={logger.slug}>
@@ -108,7 +113,6 @@ export default async function LoggersPage() {
           </div>
         </div>
         <div className="hidden w-full h-px md:block bg-zinc-800" />
-
         <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
           <div className="grid grid-cols-1 gap-4">
             {sorted
@@ -136,6 +140,55 @@ export default async function LoggersPage() {
                   <Article logger={logger} views={views[logger.slug] ?? 0} />
                 </Card>
               ))}
+          </div>
+        </div>
+        <div className="hidden w-full h-px md:block bg-zinc-800" />
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2">
+          <Card>
+            <Link href={`/loggers/${running.slug}`}>
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
+                    {running.date ? (
+                      <time dateTime={new Date(running.date).toISOString()}>
+                        {Intl.DateTimeFormat(undefined, {
+                          dateStyle: "medium",
+                        }).format(new Date(running.date))}
+                      </time>
+                    ) : (
+                      <span>LIVE UPDATING</span>
+                    )}
+                  </div>
+                  <span className="flex items-center gap-1 text-xs text-zinc-500">
+                    <Eye className="w-4 h-4" />{" "}
+                    {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+                      views[running.slug] ?? 0
+                    )}
+                  </span>
+                </div>
+                <h2
+                  id="running-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                >
+                  {running.title}
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  {running.description}
+                </p>
+                <div className="absolute bottom-4 md:bottom-8">
+                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                    Read more <span aria-hidden="true">&rarr;</span>
+                  </p>
+                </div>
+              </article>
+            </Link>
+          </Card>
+          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+            {[bottom3, bottom2].map((logger) => (
+              <Card key={logger.slug}>
+                <Article logger={logger} views={views[logger.slug] ?? 0} />
+              </Card>
+            ))}
           </div>
         </div>
       </div>
