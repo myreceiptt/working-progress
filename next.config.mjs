c470250c57e729f1dc9f2eda3807250ab6d6bfeb
myreceiptt <nota@straight-line.org>
2024-11-24 19:51:25 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   experimental: {
     mdxRs: true,
